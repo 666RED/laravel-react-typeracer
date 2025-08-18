eabf8d5952af4298e6869a3ff67829cf8b55a9b3
SEE HONG CHEN <hongchensee8@gmail.com>
2025-08-18 23:59:07 +0800

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,11 +33,11 @@ export default defineConfig<LaravelOptions>({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    laravelBaseUrl: 'http://localhost:8000/playwright',
+    laravelBaseUrl: `${process.env.BASE_URL}/playwright`,
   },
 
   /* Configure projects for major browsers */

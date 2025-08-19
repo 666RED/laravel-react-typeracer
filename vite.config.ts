cd1b95fd33a1618756acfe95505a49fec89a2dd6
SEE HONG CHEN <hongchensee8@gmail.com>
2025-08-20 02:47:44 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import { resolve } from 'node:path';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.tsx'],
       ssr: 'resources/js/ssr.tsx',
-      refresh: mode === 'production',
+      refresh: true,
     }),
     react(),
     tailwindcss(),
@@ -24,18 +24,15 @@ export default defineConfig(({ mode }) => ({
       '@': path.resolve(__dirname, 'resources/js'),
     },
   },
-  server:
-    mode === 'development'
-      ? {
-          host: '0.0.0.0',
-          port: 5173,
-          hmr: {
-            host: 'localhost',
-            port: 5173,
-          },
-          watch: {
-            usePolling: process.env.APP_URL === 'http://localhost', // only run in docker dev
-          },
-        }
-      : undefined,
-}));
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
+    watch: {
+      usePolling: process.env.APP_URL === 'http://localhost', // only run in docker dev
+    },
+  },
+});

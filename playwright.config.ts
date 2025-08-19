4272406cf6973d2bdf12acd30d632f6baba42be4
SEE HONG CHEN <hongchensee8@gmail.com>
2025-08-20 00:44:57 +0800

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, `.env`) });
+dotenv.config({ path: path.resolve(__dirname, '.env.testing') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -104,7 +104,7 @@ export default defineConfig<LaravelOptions>({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'php artisan serve',
+      command: 'php artisan serve --env=testing',
       url: process.env.APP_URL,
       reuseExistingServer: !process.env.CI,
     },

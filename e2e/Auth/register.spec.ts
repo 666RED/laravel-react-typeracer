70de170ee4384c373c18c397b780f652b8d6fb33
SEE HONG CHEN <hongchensee8@gmail.com>
2025-08-19 12:34:34 +0800

    comment tests

--- e2e/Auth/register.spec.ts
+++ e2e/Auth/register.spec.ts
@@ -1,14 +1,14 @@
 import { expect, test } from '../fixtures/index';
 
-test('Should navigate to register page', async ({ page }) => {
-  await page.goto('/');
+// test('Should navigate to register page', async ({ page }) => {
+//   await page.goto('/');
 
-  const registerNav = page.getByRole('link', { name: 'Register' });
-  await expect(registerNav).toBeVisible();
-  await registerNav.click();
-  await page.waitForURL('/register');
-  await expect(page.getByTestId('register-form')).toBeVisible();
-});
+//   const registerNav = page.getByRole('link', { name: 'Register' });
+//   await expect(registerNav).toBeVisible();
+//   await registerNav.click();
+//   await page.waitForURL('/register');
+//   await expect(page.getByTestId('register-form')).toBeVisible();
+// });
 
 test('Should register new user', async ({ page, toaster }) => {
   await page.goto('/register');
@@ -33,43 +33,43 @@ test('Should register new user', async ({ page, toaster }) => {
   await page.waitForURL('/');
 });
 
-test.describe('Should fail to register new user', () => {
-  test('Email already existed', async ({ page, toaster }) => {
-    await page.goto('/register');
+// test.describe('Should fail to register new user', () => {
+//   test('Email already existed', async ({ page, toaster }) => {
+//     await page.goto('/register');
 
-    await page.getByRole('textbox', { name: 'Name:' }).click();
-    await page.getByRole('textbox', { name: 'Name:' }).fill('Test name');
-    await page.getByRole('textbox', { name: 'Email:' }).click();
-    await page.getByRole('textbox', { name: 'Email:' }).fill('test@gmail.com');
-    await page.getByRole('textbox', { name: 'Password:' }).click();
-    await page.getByRole('textbox', { name: 'Password:' }).fill('12341234');
-    await page.getByRole('textbox', { name: 'Should match password' }).click();
-    await page.getByRole('textbox', { name: 'Should match password' }).fill('12341234');
+//     await page.getByRole('textbox', { name: 'Name:' }).click();
+//     await page.getByRole('textbox', { name: 'Name:' }).fill('Test name');
+//     await page.getByRole('textbox', { name: 'Email:' }).click();
+//     await page.getByRole('textbox', { name: 'Email:' }).fill('test@gmail.com');
+//     await page.getByRole('textbox', { name: 'Password:' }).click();
+//     await page.getByRole('textbox', { name: 'Password:' }).fill('12341234');
+//     await page.getByRole('textbox', { name: 'Should match password' }).click();
+//     await page.getByRole('textbox', { name: 'Should match password' }).fill('12341234');
 
-    await page.getByRole('button', { name: 'Register' }).click();
+//     await page.getByRole('button', { name: 'Register' }).click();
 
-    await toaster.waitFor('The email has already been taken.');
-    await expect(page.locator('p', { hasText: 'The email has already been taken.' })).toBeVisible();
-    await expect(page.getByRole('textbox', { name: 'Password:' })).toBeEmpty();
-    await expect(page.getByRole('textbox', { name: 'Should match password' })).toBeEmpty();
-  });
+//     await toaster.waitFor('The email has already been taken.');
+//     await expect(page.locator('p', { hasText: 'The email has already been taken.' })).toBeVisible();
+//     await expect(page.getByRole('textbox', { name: 'Password:' })).toBeEmpty();
+//     await expect(page.getByRole('textbox', { name: 'Should match password' })).toBeEmpty();
+//   });
 
-  test('Password != confirmed password', async ({ page }) => {
-    await page.goto('/register');
+//   test('Password != confirmed password', async ({ page }) => {
+//     await page.goto('/register');
 
-    await page.getByRole('textbox', { name: 'Name:' }).click();
-    await page.getByRole('textbox', { name: 'Name:' }).fill('Test name');
-    await page.getByRole('textbox', { name: 'Email:' }).click();
-    await page.getByRole('textbox', { name: 'Email:' }).fill('test3@gmail.com');
-    await page.getByRole('textbox', { name: 'Password:' }).click();
-    await page.getByRole('textbox', { name: 'Password:' }).fill('12341234');
-    await page.getByRole('textbox', { name: 'Should match password' }).click();
-    await page.getByRole('textbox', { name: 'Should match password' }).fill('12341235');
+//     await page.getByRole('textbox', { name: 'Name:' }).click();
+//     await page.getByRole('textbox', { name: 'Name:' }).fill('Test name');
+//     await page.getByRole('textbox', { name: 'Email:' }).click();
+//     await page.getByRole('textbox', { name: 'Email:' }).fill('test3@gmail.com');
+//     await page.getByRole('textbox', { name: 'Password:' }).click();
+//     await page.getByRole('textbox', { name: 'Password:' }).fill('12341234');
+//     await page.getByRole('textbox', { name: 'Should match password' }).click();
+//     await page.getByRole('textbox', { name: 'Should match password' }).fill('12341235');
 
-    await page.getByRole('button', { name: 'Register' }).click();
+//     await page.getByRole('button', { name: 'Register' }).click();
 
-    await expect(page.locator('p', { hasText: 'The password field confirmation does not match.' })).toBeVisible();
-    await expect(page.getByRole('textbox', { name: 'Password:' })).toBeEmpty();
-    await expect(page.getByRole('textbox', { name: 'Should match password' })).toBeEmpty();
-  });
-});
+//     await expect(page.locator('p', { hasText: 'The password field confirmation does not match.' })).toBeVisible();
+//     await expect(page.getByRole('textbox', { name: 'Password:' })).toBeEmpty();
+//     await expect(page.getByRole('textbox', { name: 'Should match password' })).toBeEmpty();
+//   });
+// });

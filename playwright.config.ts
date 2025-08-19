cc45b62768c23f8709a66b8be77115ff7cabbfc5
SEE HONG CHEN <hongchensee8@gmail.com>
2025-08-20 01:46:57 +0800

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, '.env.testing') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
